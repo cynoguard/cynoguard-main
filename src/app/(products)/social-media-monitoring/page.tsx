7b04fc396d0c8d6fb079454734dd2007db6462a6
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-02-08 23:44:26 +0530

    add navbar and footer to social media monitoring landing

--- src/app/(products)/social-media-monitoring/page.tsx
+++ src/app/(products)/social-media-monitoring/page.tsx
@@ -1,16 +1,20 @@
 
-import SocialMonitoringHero from "@/components/landing/social-media-monitoring/Hero"
-import FeatureGrid from "@/components/landing/social-media-monitoring/FeatureGrid"
-import HowItWorks from "@/components/landing/social-media-monitoring/HowItWorks"
 import SocialMonitoringFAQ from "@/components/landing/social-media-monitoring/FAQ"
+import FeatureGrid from "@/components/landing/social-media-monitoring/FeatureGrid"
+import SocialMonitoringHero from "@/components/landing/social-media-monitoring/Hero"
+import HowItWorks from "@/components/landing/social-media-monitoring/HowItWorks"
+import Footer from "@/components/shared/Footer"
+import Navbar from "@/components/shared/NavBar"
 
 const Page = () => {
   return (
     <div>
+      <Navbar/>
       <SocialMonitoringHero />
       <FeatureGrid />
       <HowItWorks />
       <SocialMonitoringFAQ />
+      <Footer/>
     </div>
   )
 }
